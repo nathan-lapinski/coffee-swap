178534be5ec2b0962197d872bd9f14cd32991522
nate <n4t311@gmail.com>
2022-06-04 17:01:36 +0900

    feat: basic CRA setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        ☕️Welcome to CoffeeSwap🔷
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
