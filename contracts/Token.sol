ba87295cfd1a4c614b02363376bba2437c24fd41
nate <n4t311@gmail.com>
2022-07-13 09:11:57 +0900

    refactor: cleans up Token.sol and adds tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,22 +13,5 @@ contract Token is ERC20 {
     // mint initially supply of tokens and give them to token creator
     _mint(msg.sender, initialSupply);
   }
-
-  // test function for minting miner rewards
-  function mintMinerReward() public {
-    _mint(block.coinbase, 1000);
-  }
-
-  function getTotalSupply() external view returns (uint256) {
-    return totalSupply();
-  }
-
-  function getBalanceOf(address addr) external view returns (uint256) {
-    return balanceOf(addr);
-  }
-
-  function faucet() public {
-    transfer(msg.sender, 500);
-  }
 }
 
